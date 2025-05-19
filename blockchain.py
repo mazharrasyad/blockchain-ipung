01c9734c03e6383c8df6e4240ca2b44f785332e6
mazharrasyad <muhazharrasyad@gmail.com>
2025-05-19 15:43:34 +0700

    part 2

--- blockchain.py
+++ blockchain.py
@@ -46,3 +46,43 @@ class Blockchain(object):
         content_hash = hashlib.sha256(content).hexdigest()
 
         return content_hash[:len(self.difficulty_target)] == self.difficulty_target
+    
+    def append_block(self, nonce, hash_of_previous_block):
+        block = {
+            'index' : len(self.chain),
+            'timestamp' : time(),
+            'transactions' : self.current_transactions,
+            'nonce' : nonce,
+            'hash_of_previous_block' : hash_of_previous_block
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+    
+    def add_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'amount' : amount,
+            'recipient' : recipient,
+            'sender' : sender
+        })
+        return self.last_block['index'] + 1
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+    
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', "")
+
+blockchain = Blockchain()
+
+# routes
+@app.route('/blockchain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain),
+    }
