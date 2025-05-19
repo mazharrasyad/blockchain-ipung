a206809e78d61ee07a7856d6cbc4322528b522e8
mazharrasyad <muhazharrasyad@gmail.com>
2025-05-19 16:26:36 +0700

    part 3

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Langkah
+# Langkah 1 Node
 - pip3 install flask
 - pip3 install request
 - touch blockchain.py
+
+# Penggunan
+- [GET] http://localhost:5000/blockchain
+- [GET] http://localhost:5000/mine
+- [POST] http://localhost:5000/transactions/new
--- blockchain.py
+++ blockchain.py
@@ -33,7 +33,7 @@ class Blockchain(object):
             nonce = self.proof_of_work(0, genesis_hash, [])
         )
 
-    def proof_of_work(self, index, hash_of_previous_block, transactions, nonce):
+    def proof_of_work(self, index, hash_of_previous_block, transactions):
         nonce = 0
 
         while self.valid_proof(index, hash_of_previous_block, transactions, nonce) is False:
@@ -86,3 +86,52 @@ def full_chain():
         'chain' : blockchain.chain,
         'length' : len(blockchain.chain),
     }
+
+    return jsonify(response), 200
+
+@app.route('/mine', methods=['GET'])
+def mine_block():
+    blockchain.add_transaction(
+        sender = "0",
+        recipient = node_identifier,
+        amount = 1,
+    )
+
+    last_block_hash = blockchain.hash_block(blockchain.last_block)
+
+    index = len(blockchain.chain)
+    nonce = blockchain.proof_of_work(index, last_block_hash, blockchain.current_transactions)
+
+    block = blockchain.append_block(nonce, last_block_hash)
+    response = {
+        'message' : "Block baru telah ditambahkan (mined)",
+        'index' : block['index'],
+        'hash_of_previous_block' : block['hash_of_previous_block'],
+        'nonce' : block['nonce'],
+        'transactions' : block['transactions']
+    }
+
+    return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    values = request.get_json()
+
+    required_fields = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required_fields):
+        return ('Missing fields', 400)
+    
+    index = blockchain.add_transaction(
+        values['sender'],
+        values['recipient'],
+        values['amount']
+    )
+
+    response = { 'message' : f'Transaction akan ditambahkan ke block {index}'}
+    return (jsonify(response), 201)
+
+# code di atas contoh transaksi mengirimkan uang digital dengan menggunakan blockchain
+
+# server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(sys.argv[1]))
