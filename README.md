e7dcd527cbe7a9bc97cc9040a9d7c8d4a5323817
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2025-05-25 11:03:03 +0700

    part 4 sinkronisasi

--- README.md
+++ README.md
@@ -5,11 +5,20 @@
 - pip3 install flask
 - pip3 install request
 - touch blockchain.py
-
-# Penggunan
 - python3 blockchain.py 5000
 - [GET] http://localhost:5000/blockchain
 - [GET] http://localhost:5000/mine
 - [POST] http://localhost:5000/transactions/new
 
 # Blockchain Sinkronisasi
+
+- python3 blockchain.py 5000
+- python3 blockchain.py 5001
+- [POST] http://localhost:5000/transactions/new
+- [GET] http://localhost:5000/blockchain
+- [GET] http://localhost:5001/blockchain
+- [GET] http://localhost:5000/mine
+- [POST] http://localhost:5000/nodes/add_nodes tambah 5001
+- [POST] http://localhost:5001/nodes/add_nodes tambah 5000
+- [GET] http://localhost:5000/nodes/sync
+- [GET] http://localhost:5001/nodes/sync
--- blockchain.py
+++ blockchain.py
@@ -35,10 +35,57 @@ class Blockchain(object):
             nonce = self.proof_of_work(0, genesis_hash, [])
         )
 
+    # sinkronisasi blockchain
     def add_node(self, address):
         parse_url = urlparse(address)
         self.nodes.add(parse_url.netloc)
         print(parse_url.netloc)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            
+            if block['hash_of_previous_block'] != self.hash_block(last_block):
+                return False
+            
+            if not self.valid_proof(
+                current_index,
+                block['hash_of_previous_block'],
+                block['transactions'],
+                block['nonce']): 
+                return False
+
+            last_block = block
+            current_index += 1
+        
+        return True
+    
+    def update_blockchain(self):
+        neighbours = self.nodes
+        new_chain = None
+        
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/blockchain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # cek yang paling panjang
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+                if new_chain:
+                    self.chain = new_chain
+                    return True
+                
+        return False    
 
     def proof_of_work(self, index, hash_of_previous_block, transactions):
         nonce = 0
@@ -137,7 +184,39 @@ def new_transactions():
     response = { 'message' : f'Transaction akan ditambahkan ke block {index}'}
     return (jsonify(response), 201)
 
-# code di atas contoh transaksi mengirimkan uang digital dengan menggunakan blockchain
+@app.route('/nodes/add_nodes', methods=['POST'])
+def add_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+
+    if nodes is None:
+        return "Error, missing node(s) info", 400
+    
+    for node in nodes:
+        blockchain.add_node(node)
+
+    response = {
+        'message' : "Node baru telah di tambahkan",
+        'nodes' : list(blockchain.nodes)
+    }
+
+    return jsonify(response), 200
+
+@app.route('/nodes/sync', methods=['GET'])
+def sync():
+    updated = blockchain.update_blockchain()
+    if updated:
+        response = {
+            'message': "Blockchain telah diupdate dengan data terbaru",
+            'blockchain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': "Blockchain sudah menggunakan data paling baru",
+            'blockchain': blockchain.chain
+        }
+    
+    return jsonify(response), 200
 
 # server
 if __name__ == '__main__':
